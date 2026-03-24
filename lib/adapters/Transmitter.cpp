78e2f58b7c9432a7c5a1ae19bdaa0ba6a69ef248
viq hulow <viq.hlw@gmail.com>
2026-03-24 10:14:18 +0100

    feat: transmit data without callback

--- lib/adapters/Transmitter.cpp
+++ lib/adapters/Transmitter.cpp
@@ -1,6 +1,7 @@
 #include "Transmitter.h"
 #include <iostream>
 #include <functional>
+#include "freertos/FreeRTOS.h"
 
 Transmitter::Transmitter(rmt_channel_handle_t channel) : _channel(channel) {
     _encoderConfigs = {};
@@ -9,4 +10,22 @@ Transmitter::Transmitter(rmt_channel_handle_t channel) : _channel(channel) {
     _streamConfigs.loop_count = 0;
 }
 
-void Transmitter::transmit(const std::vector<rmt_symbol_word_t>& symbols) {}
+void Transmitter::transmit(const std::vector<rmt_symbol_word_t>& symbols) {
+    rmt_new_copy_encoder(&_encoderConfigs, &_streamEncoder);
+    esp_err_t err = rmt_transmit(
+        _channel,
+        _streamEncoder,
+        symbols.data(),
+        symbols.size() * sizeof(rmt_symbol_word_t),
+        &_streamConfigs
+    );
+
+    if (err != ESP_OK) {
+        printf("rmt_transmit failed: %d\n", err);
+    }
+
+    _streamEncoder = nullptr;
+
+    rmt_tx_wait_all_done(_channel, portMAX_DELAY);
+    rmt_del_encoder(_streamEncoder);
+}
