1727ead0361b9ead8865b2b623dcebda18cae6bc
viq hulow <viq.hlw@gmail.com>
2026-03-25 15:07:41 +0100

    feat: run 2 strip async

--- src/main.cpp
+++ src/main.cpp
@@ -4,80 +4,79 @@
 #include "ConfigsBuilder.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <string>
+#include <sstream>
 
-/* Should be a class ?*/
+/* Task arguments */
 struct TaskArgs {
     CommandHandler* handler;
-    Command command;
     std::string msg;
 };
 
+/* Task function toggling two commands infinitely */
 void task(void* param) {
     auto* args = static_cast<TaskArgs*>(param);
 
-    // while(true) {
-        args->handler->execute(args->command, [args]() {
+    Command cmdOff   = Command::from(0, 0, 0, 60);
+    Command cmdGreen = Command::from(0, 255, 0, 60);
+
+    std::function<void(bool)> executeNext;
+    executeNext = [args, &executeNext, cmdOff, cmdGreen](bool isGreen) {
+        Command current = isGreen ? cmdGreen : cmdOff;
+
+        args->handler->execute(current, [args, &executeNext, isGreen]() {
             Logger logger;
-            logger.info("MAIN", args->msg);
+            std::stringstream ss;
+            ss << args->msg << " - " << (isGreen ? "Green" : "Off");
+            logger.info("MAIN", ss.str());
+
+            // Delay between toggles
+            // vTaskDelay(pdMS_TO_TICKS(500));
+
+            // Trigger opposite command
+            executeNext(!isGreen);
         });
+    };
+
+    // Start with LEDs off
+    executeNext(false);
+
+    // Task itself just idles; callbacks handle toggling
+    while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
-    // }
+    }
 
     delete args;
     vTaskDelete(NULL);
 }
 
-
 extern "C" void app_main() {
     Logger logger;
+
+    // Base configuration
     ConfigsBuilder baseConfigs = ConfigsBuilder()
         .clock(RMT_CLK_SRC_DEFAULT)
-        .memBlockSymbols(128) // 1 LED = 24 symbols / 60 LEDs = 1440 symbols
+        .memBlockSymbols(128) // memory for LEDs
         .queueDepth(4)
         .resolutionHz(10'000'000);
 
+    // First LED strip (GPIO4)
     ConfigsBuilder configsOne = baseConfigs.gpioNum(GPIO_NUM_4);
-
     auto* transceiverOne = new Rmt(configsOne.build());
     CommandHandler handlerOne(logger, *transceiverOne);
-    Command commandOne = Command::from(0, 0, 0, 60);
+    auto* argsOne = new TaskArgs{ &handlerOne, "LED Strip 1" };
 
+    // Second LED strip (GPIO5)
     ConfigsBuilder configsTwo = baseConfigs.gpioNum(GPIO_NUM_5);
     auto* transceiverTwo = new Rmt(configsTwo.build());
     CommandHandler handlerTwo(logger, *transceiverTwo);
-    Command commandTwo = Command::from(0, 0, 0, 60);
+    auto* argsTwo = new TaskArgs{ &handlerTwo, "LED Strip 2" };
 
-    auto* argsOne = new TaskArgs{
-        &handlerOne, 
-        commandOne, 
-        "Main one triggered"
-    };
+    // Create FreeRTOS tasks
+    xTaskCreate(task, "handlerOneTask", 4096, argsOne, 5, NULL);
+    xTaskCreate(task, "handlerTwoTask", 4096, argsTwo, 5, NULL);
 
-    auto* argsTwo = new TaskArgs{
-        &handlerTwo, 
-        commandTwo, 
-        "Main two triggered"
-    };
-
-    /* Check xTaskCreate */
-    xTaskCreate(
-        task,                 // Task function pointer
-        "handlerOneTask",     // Name
-        4096,                 // Stack size
-        argsOne,              // Parameter
-        5,                    // Priority
-        NULL                  // Task handle
-    );
-
-    xTaskCreate(
-        task,                 // Task function pointer
-        "handlerTwoTask",     // Name
-        4096,                 // Stack size
-        argsTwo,              // Parameter
-        5,                    // Priority
-        NULL                  // Task handle
-    );
-
+    // Main loop does nothing; tasks handle LEDs
     while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
