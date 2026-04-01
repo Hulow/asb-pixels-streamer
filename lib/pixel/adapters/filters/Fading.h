92a867e1df01b0fc110caa80377b8e5b60045487
viq hulow <viq.hlw@gmail.com>
2026-04-01 12:24:00 +0200

    feat: clean fadding

--- lib/pixel/adapters/filters/Fading.h
+++ lib/pixel/adapters/filters/Fading.h
@@ -15,13 +15,29 @@ class Fading : public Filter {
         Fading(IConsumer& consumer, ITimer& timer, const float& factor) : Filter(consumer, timer), _factor(factor) {}
         void stream(Frame& frame) override {
             auto originalPixels = frame.getPixels();
-            for (float f = 0; f <= 1; f += _factor) {
-                for (size_t i = 0; i < frame.getPixels().size(); ++i) {
-                    frame.getPixels()[i] = originalPixels[i];   // reset to original
-                    frame.getPixels()[i].applyBrightness(f);    // scale brightness
-                }
+            fadeOut(frame, originalPixels);
+        }
+
+        void fadeIn(Frame& frame, const std::vector<Pixel>& originalPixels) {
+            for (float factor = 0; factor <= 1.0f; factor += _factor) {
+                applyBrightness(frame, originalPixels, factor);
                 _consumer.stream(frame);
                 _timer.wait(100);
+            }
+        }
+
+        void fadeOut(Frame& frame, const std::vector<Pixel>& originalPixels) {
+            for (float factor = 1.0f; factor >= 0.0f; factor -= _factor) {
+                applyBrightness(frame, originalPixels, factor);
+                _consumer.stream(frame);
+                _timer.wait(100);
+            }
+        }
+
+        void applyBrightness(Frame& frame, const std::vector<Pixel>& originalPixels, float factor) {
+            for (size_t i = 0; i < frame.getPixels().size(); ++i) {
+                frame.getPixels()[i] = originalPixels[i]; 
+                frame.getPixels()[i].applyBrightness(factor);
             }
         }
 };
