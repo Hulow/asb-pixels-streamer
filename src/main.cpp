b40cb75b490e5e585734e19a5c974a80f8ff8a1d
viq hulow <viq.hlw@gmail.com>
2026-04-12 12:21:05 +0200

    chore: clean main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "driver/rmt_tx.h"
 
 #include "../lib/shared/Timer.h"
-#include "../lib/shared/TaskScheduler.h"
 
 #include "../lib/pixel/drivers/ConfigsBuilder.h"
 #include "../lib/pixel/drivers/TimingBuilder.h"
@@ -22,20 +21,7 @@
 #include "../lib/pixel/services/ChasingService.h"
 
 
-struct Params {
-    CommandHandler* handler;
-    Command command;
-};
-
-void runTask(void* arg) {
-    auto params = static_cast<Params*>(arg);
-    params->handler->execute(params->command);
-}
-
 extern "C" void app_main() {
-    WifiManager wifiManager;
-    wifiManager.start();
-    
     ConfigsBuilder baseConfigs = ConfigsBuilder()
         .clock(RMT_CLK_SRC_APB)
         .memBlockSymbols(256)
@@ -50,21 +36,10 @@ extern "C" void app_main() {
         .resetTime(400000)
         .build();
 
-    const int LEDS_COUNT = 13;
-    Command command = Command::from(0, 255, 0, LEDS_COUNT);
     Timer timer;
 
-    /* ---- CHANNEL 1 ---- */
-    // Transmitter* transmitterOne = new Transmitter(
-    //     baseConfigs.gpioNum(GPIO_NUM_5).build(), 
-    //     timingConfigs
-    // );
-    // Blinking* blinkingEffectOne = new Blinking(*transmitterOne);
-    // Chasing* chasingOne = new Chasing(*blinkingEffectOne);
-    // CommandHandler* handlerOne = new CommandHandler(
-    //     *chasingOne, 
-    //     *timer
-    // );
+    /* ---- CHANNEL 5 ---- */
+
      Transmitter transmitterOne(
         baseConfigs.gpioNum(GPIO_NUM_5).build(), 
         timingConfigs
@@ -80,6 +55,13 @@ extern "C" void app_main() {
     ChasingController controller(service);
     ChasingRoute route(controller);
 
+    /* ---- Start WIFI Network ---- */
+
+    WifiManager wifiManager;
+    wifiManager.start();
+
+    /* ---- Start Web Server ---- */
+
     std::vector<IRoute*> routes = {
         &route
     };
@@ -87,48 +69,6 @@ extern "C" void app_main() {
     WebServer webServer(routes);
     webServer.start();
     webServer.registerRoutes();
-
-
-    // Params* paramsOne = new Params{
-    //     handlerOne, 
-    //     command
-    // };
-
-    // TaskScheduler* task1 = new TaskScheduler(
-    //     runTask, 
-    //     "Handler on channel 5", 
-    //     paramsOne
-    // );
-    // task1->start();
-
-    // /* ---- CHANNEL 2 ---- */
-    // Transmitter* transmitterTwo = new Transmitter(
-    //     baseConfigs.gpioNum(GPIO_NUM_4).build(), 
-    //     timingConfigs
-    // );
-
-    // /* Wrap in Fading */
-    // Fading* fadingEffect = new Fading(*transmitterTwo);
-
-    // /* Wrap in Chasing */
-    // Chasing* chasingEffect = new Chasing(*fadingEffect);
-
-    // /* Wrap in Blinking */
-    // Blinking* blinkingEffectTwo = new Blinking(*chasingEffect);
-
-    // /* Use the chain in command handler */
-    // CommandHandler* handlerTwo = new CommandHandler(*blinkingEffectTwo, *timer);
-
-    // Params* params2 = new Params{
-    //     handlerTwo, 
-    //     command
-    // };
-    // TaskScheduler* task2 = new TaskScheduler(
-    //     runTask, 
-    //     "Handler on channel 4", 
-    //     params2
-    // );
-    // task2->start();
 
      while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
